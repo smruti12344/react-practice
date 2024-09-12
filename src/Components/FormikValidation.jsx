a6a1b76fb55814c7acd06578485732462ce94e33
smruti12344 <72293400+smruti12344@users.noreply.github.com>
2024-09-12 09:23:37 +0530

    here we added validationfor form  using formik validattion process

--- src/Components/FormikValidation.jsx
+++ src/Components/FormikValidation.jsx
@@ -9,8 +9,35 @@ export default function FormikValidation() {
     },
     onSubmit: (values)=>{
       alert(JSON.stringify(values))
-    }
+    },
+    validate:userDetailsVerify
   })
+  function userDetailsVerify(userDetails){
+    const errors={};
+    if(userDetails.userName==''){
+      errors.userName="username required";
+    }
+    if (!/^[a-zA-Z][a-zA-Z0-9!@#$%^&*()_+]*$/.test(userDetails.userName)) {
+      errors.userName ="user name is invalid"
+    }
+    if (!userDetails.Password) {
+      errors.Password = 'Password is required';
+    } else if (userDetails.Password.length < 8) {
+      errors.Password = 'Password must be at least 8 characters long';
+    } else if (!/[A-Z]/.test(userDetails.Password)) {
+      errors.Password = 'Password must contain at least one uppercase letter';
+    } else if (!/[a-z]/.test(userDetails.Password)) {
+      errors.Password = 'Password must contain at least one lowercase letter';
+    } else if (!/[0-9]/.test(userDetails.Password)) {
+      errors.Password = 'Password must contain at least one number';
+    } else if (!/[!@#$%^&*()_+]/.test(userDetails.Password)) {
+      errors.Password = 'Password must contain at least one special character';
+    }
+    if(userDetails.city==''){
+      errors.city="please select one"
+    }
+    return errors;
+  }
   return (
     <div className="container-fluid">
         <form onSubmit={formik.handleSubmit}>
@@ -19,15 +46,18 @@ export default function FormikValidation() {
                 <dt>User Name</dt>
                 <dd><input type="text" name="userName" onChange={formik.handleChange} value={formik.values.userName}  />
                 </dd>
+                <dd className='text-bg-danger'>{formik.errors.userName}</dd>
                 <dt>Password</dt>
                 <dd><input type="password" name="Password" onChange={formik.handleChange} value={formik.values.Password} />
                 </dd>
+                <dd className='text-bg-danger'>{formik.errors.Password}</dd>
                 <dt>Select City</dt>
                 <dd><select name='city' onChange={formik.handleChange} value={formik.values.city}>
                     <option value="any">Choose..</option>
                     <option value="odisha">Odi</option>
                     <option value="hydrabad">Hyd</option>
                     </select></dd>
+                    <dd className='text-bg-danger'>{formik.errors.city}</dd>
             </dl>
             <button>submit</button>
         </form>
